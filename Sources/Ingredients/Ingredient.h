35c7c80fe05dcf6169829ca06a9908d95289c6fa
LeonWongInspiration <leonwong.inspiration@gmail.com>
2018-10-29 17:15:34 +0800

    Basic Ingredient class

--- Sources/Ingredients/Ingredient.h
+++ Sources/Ingredients/Ingredient.h
@@ -5,4 +5,21 @@
 #ifndef DESPAT_MYKITCHEN_INGREDIENT_H
 #define DESPAT_MYKITCHEN_INGREDIENT_H
 
+#include "../Object.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+
+class Ingredient: public Object {
+public:
+    /**
+     * @brief Return the basic info of this object: name and address.
+     *
+     * @return (string) A string about this object and its address.
+     */
+    inline virtual std::string who_am_i() const;
+
+
+};
+
 #endif //DESPAT_MYKITCHEN_INGREDIENT_H
