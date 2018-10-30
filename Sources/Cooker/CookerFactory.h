7082f506a3900a6c7966d45b0edc487c89fffaee
baziing <sarumisarumi@hotmail.com>
2018-10-31 03:47:53 +0800

    Create CookerFactory.h

---
+++ Sources/Cooker/CookerFactory.h
@@ -0,0 +1,55 @@
+#pragma once
+#ifndef CookerFactory_h
+#define CookerFactory_h
+
+class Cooker;
+
+/**
+ * @brief CookerFactory produces Cooker.
+ *
+ */
+class CookerFactory
+{
+public:
+    CookerFactory();
+    ~CookerFactory();
+    virtual Cooker* CreateCooker() = 0;
+};
+
+/**
+ * @brief PotFactory produces Pot.
+ *
+ */
+class PotFactory :public CookerFactory
+{
+public:
+    PotFactory();
+    ~PotFactory();
+    Cooker* CreateCooker();
+};
+
+/**
+ * @brief KitchenKnifeFactory produces KitchenKnife.
+ *
+ */
+class KitchenKnifeFactory :public CookerFactory
+{
+public:
+    KitchenKnifeFactory();
+    ~KitchenKnifeFactory();
+    Cooker* CreateCooker();
+};
+
+/**
+ * @brief OvenFactory produces Oven.
+ *
+ */
+class OvenFactory :public CookerFactory
+{
+public:
+    OvenFactory();
+    ~OvenFactory();
+    Cooker* CreateCooker();
+};
+
+#endif
