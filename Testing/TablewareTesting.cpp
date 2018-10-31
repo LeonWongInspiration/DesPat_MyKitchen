dd2cce8beb2446c57bbd6bb7b1f446251abe8ea7
ZhangQ24 <ZhangQ24@users.noreply.github.com>
2018-10-31 11:34:02 +0800

    factory pattern

---
+++ Testing/TablewareTesting.cpp
@@ -0,0 +1,26 @@
+﻿//
+//  main.cpp
+//  Created by Zhang Qian on 2018/10/31.
+//
+
+#include "../Sources/Tableware/DishUp.h"
+
+#include <stdlib.h>
+
+#include <time.h>
+
+int main()
+{
+	srand(time(0));
+
+	int num = rand() % 1000;
+
+	std::cout<<"菜的数量为" << num<<std::endl;
+	
+	DishUp dishUp;
+
+	dishUp.DishUpFood(num);
+
+	getchar();
+	return 0;
+}
