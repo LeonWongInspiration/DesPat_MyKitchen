48ccd0a612882e8d8c35816eaa23a0889883d70f
yangtzi <2214821650@qq.com>
2018-10-31 08:35:48 +0800

    Creat Ingredient.cpp and Ingredient.h without map

--- Sources/Ingredients/Ingredient.cpp
+++ Sources/Ingredients/Ingredient.cpp
@@ -1,13 +1,66 @@
 //
 // Created by Leon Wong on 29/Oct.
 //
-
 #include "Ingredient.h"
+#include<iostream>
+#include<stdio.h>
 
-inline std::string Ingredient::who_am_i() const {
-    std::string ret = "Ingredient@";
-    std::stringstream ss;
+using namespace std;
+
+inline string Ingredient::who_am_i() const {
+    string ret = "Ingredient@";
+    stringstream ss;
     ss << this;
     ret += ss.str();
     return ret;
 }
+
+Ingredient::Ingredient() {};
+
+Ingredient::Ingredient(string n){
+	Ingredient.name = n;
+}
+
+Ingredient::~Ingredient() {};
+
+string Ingredient::get_name(const Ingredient* i) {
+	return i->name;
+}
+
+bool Ingredient::can_be_cooked(const Ingredient* i) { return 1; }
+bool Ingredient::can_be_cut(const Ingredient* i) { return 1; }
+bool Ingredient::can_be_fried(const Ingredient* i) { return 1; }
+bool Ingredient::can_be_steamed(const Ingredient* i) { return 1; }
+
+Ingredient* Ingredient::get_ingredient(Ingredient& i, double w) {
+	Ingredient* p = new Ingredient(i);
+	p->weight =w;
+	i.weight -= w;
+	return p;
+}
+
+Vegetable::Vegetable() {};
+Vegetable::Vegetable(string n) {
+	Vegetable.name = n;
+}
+Vegetable::~Vegetable() {};
+
+Meat::Meat() {};
+Meat::Meat(string n) {
+	Meat.name = n;
+}
+Meat::~Meat() {};
+
+Fruit::Fruit() {};
+Fruit::Fruit(string n) {
+	Fruit.name = n;
+}
+Fruit::~Fruit() {};
+
+Flavour::Flavour() {};
+Flavour::Flavour(string n) {
+	Flavour.name = n;
+}
+Flavour::~Flavour() {};
+
+
--- Sources/Ingredients/Ingredient.h
+++ Sources/Ingredients/Ingredient.h
@@ -6,12 +6,21 @@
 #define DESPAT_MYKITCHEN_INGREDIENT_H
 
 #include "../Object.h"
+#include <stdio.h>
 #include <iostream>
 #include <string>
 #include <sstream>
+using namespace std;
 
 class Ingredient: public Object {
 public:
+	/**
+	* @brief construct a kind of ingredient.
+	*
+	*/
+	Ingredient();
+	Ingredient(string n);
+	~Ingredient();
     /**
      * @brief Return the basic info of this object: name and address.
      *
@@ -19,10 +28,65 @@ public:
      */
     inline virtual std::string who_am_i() const override;
 
+	/**
+     * @brief Return the name of this ingredient.
+     *
+     */
+	string get_name(const Ingredient* i);
+
+	/**
+	* @brief Return the state of this ingredient.
+	*
+	*/
+	bool can_be_cut(const Ingredient* i);
+	bool can_be_fried(const Ingredient* i);
+	bool can_be_cooked(const Ingredient* i);
+	bool can_be_steamed(const Ingredient* i);
+
+	/**
+	* @brief Return a new object that is a definite food.
+	*
+	*/
+	Ingredient* get_ingredient(Ingredient& i, double w);
+
+
 protected:
-    std::string color;
-    std::string shape;
+    string name;
+    double weight;
     // TODO: set some properties for ingredients
+	char state = '11111111';
+};
+
+class Vegetable :public Ingredient {
+	Vegetable();
+	Vegetable(string n);
+	~Vegetable();
+};
+
+class Fruit :public Ingredient {
+	Fruit();
+	Fruit(string n);
+	~Fruit();
+};
+
+class Meat :public Ingredient {
+	Meat();
+	Meat(string n);
+	~Meat();
+};
+
+class Flavour :public Ingredient {
+	Flavour();
+	Flavour(string n);
+	~Flavour();
+
+protected:
+	const char state = '00001111';
 };
 
 #endif //DESPAT_MYKITCHEN_INGREDIENT_H
+
+
+
+
+
